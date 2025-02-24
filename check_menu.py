d54c7b328f9daefe22025d77364a150938061738
Pavan Thanuj Upadrasta <49611451+n00bcodr@users.noreply.github.com>
2025-02-24 12:21:13 +0000

    Update check_menu.py

--- check_menu.py
+++ check_menu.py
@@ -5,7 +5,7 @@ import hashlib
 import datetime
 
 # Define the URL of the page
-url = "https://www.ericssondining.ie/post/weekly-menu-07-02-2025"
+url = "https://www.ericssondining.ie/post/weekly-menu-24-02-2025"
 
 # Define the path to save the image
 img_path = "weekly_menu.jpg"  # Save to the root of the repo
